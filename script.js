745bdc71adf819b91901b4c3ed9170deff6669d4
Galina Prushenova <ayuraria@gmail.com>
2023-02-22 00:56:28 +0800

    week10

---
+++ script.js
@@ -0,0 +1,16 @@
+//задание 1
+function showMessage() {
+	console.log("Я учу Java Script!");
+}
+showMessage()
+
+//задание 2
+function changeImg(){
+	let image = document.getElementById('cat');
+	image.src = "cat2.jpg"
+}
+
+function changeImg2() {
+	let image = document.getElementById('cat');
+	image.src = "cat1.jpg"
+}
